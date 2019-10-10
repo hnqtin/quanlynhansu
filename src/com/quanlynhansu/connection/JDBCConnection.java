2b69cd7c1acb10e68d928ed8e4487fd6df8c765a
= <=>
2019-10-10 13:40:15 +0700

    commit to repository

---
+++ src/com/quanlynhansu/connection/JDBCConnection.java
@@ -0,0 +1,30 @@
+package com.quanlynhansu.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//ket noi voi DB
+public class JDBCConnection {
+	private static String url = "jdbc:mysql://localhost:3306/crm";
+	private static String username = "root";
+	private static String password = "123456";
+
+	public static Connection getConnection() {
+		
+		// Su dung lop Driver cua JDBC
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(url, username, password);
+			 
+			return connection;
+			
+		} catch (ClassNotFoundException e) {
+		 
+		} catch (SQLException e) {
+			 
+		}
+		
+		return null;
+	}
+}
